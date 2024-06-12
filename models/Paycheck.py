3f536b86fc0c8c0abf97ebf78ae96179d0f0c5cd
Dani Belau <belau010@umn.edu>
2024-06-12 09:02:04 -0500

    added Clock, Paycheck and Shift models

--- models/Paycheck.py
+++ models/Paycheck.py
@@ -4,8 +4,8 @@ def initializePaycheckModel(db):
     class Paycheck(db.Model):
         id = db.Column(db.Integer, primary_key = True)
         workerId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-        startDate = db.Column(db.DateTime, nullable = False)
-        endDate = db.Column(db.DateTime, nullable = False)
+        startDate = db.Column(db.Date, nullable = False)
+        endDate = db.Column(db.Date, nullable = False)
         hours = db.Column(db.Float, nullable = False, default = 0.0)
         total = db.Column(db.Numeric(10, 2), nullable = False)
             
