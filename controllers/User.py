4fe90a42af1a02730c96da90c42d60f45a5f6f83
Dani Belau <belau010@umn.edu>
2024-07-17 16:20:41 -0500

    see comments in the log in fucntion of the User.py controller

--- controllers/User.py
+++ controllers/User.py
@@ -25,16 +25,19 @@ def initializeUserController(app, db):
             print(str(e))
             return jsonify({"success":False,"message":str(e)})
 
-    @app.route('/user/logIn', methods = ['POST'])
+    @app.route('/user/logIn', methods = ['POST']) 
+    # the above line dictates that the below function fires when this API endpoint
+    # is hit from the front end (aka the View) using the POST method
     def logIn():
         email = request.json.get("email")
         password = request.json.get("password")
-
+        # we get the data from the HTTP post request for the email and password
         user = User.query.filter_by(email=email).first()
         # if the user exists and the passwords match, save the user to the session
+        # instead of having to make an SQL query, we leverage MySQL alchemy to simplify
         if user and check_password_hash(user.password, password):
-            del user.password
             session["user"] = {"email": user.email, "firstName": user.firstName, "lastName": user.lastName, "password": user.password, "noShowDays": user.noShowDays, "CallOutDays": user.callOutDays, "id": user.id, "payRate": user.payRate}
+            # return a response to the View that the log in was successful as well as some user information
             return jsonify({"success":True,"user": session["user"]})
         else:
             return jsonify({"success":False,"message": "Incorrect Username or Password"})
